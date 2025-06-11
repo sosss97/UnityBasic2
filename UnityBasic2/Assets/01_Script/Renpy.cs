dc6b00cc05e060aa6388413c80c819e6d4168d0b
xince97 <xince97@gmail.com>
2025-06-11 13:14:55 +0900

    [Add] 중간저장
    점심시간 저장

---
+++ UnityBasic2/Assets/01_Script/Renpy.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Renpy : MonoBehaviour
+{
+    [SerializeField] Image img_BG; // 배경
+    [SerializeField] Image[] img_Character; // 캐릭터
+
+    [SerializeField] TextMeshProUGUI txt_Name; // 캐릭터 이름 : 호시노
+    [SerializeField] TextMeshProUGUI txt_NameTitle; // 캐릭터 타이틀 : 대책위원회
+    [SerializeField] TextMeshProUGUI txt_Dialogue; // 대사 : 대사창
+
+   
+}
